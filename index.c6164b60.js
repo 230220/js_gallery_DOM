7a0bbd8be256e56425f0fbc35282d92ae0476dbc
annakalmazan <klavachichikova@gmail.com>
2025-09-05 14:21:03 +0200

    make build

---
+++ index.c6164b60.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest("a");if(n){e.src=n.href;var a=n.querySelector("img");e.alt=(null==a?void 0:a.alt)||"main image"}})});
+//# sourceMappingURL=index.c6164b60.js.map
