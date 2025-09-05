9c9df0cc71497fdc5b16df622374baae69abe148
annakalmazan <klavachichikova@gmail.com>
2025-09-05 14:14:52 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bigPicture = document.getElementById('largeImg');
+  const smallPicture = document.getElementById('thumbs');
+
+  smallPicture.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    const link = ev.target.closest('a');
+
+    if (!link) {
+      return;
+    }
+    bigPicture.src = link;
+    bigPicture.alt = link.title || 'main image';
+  });
+});
